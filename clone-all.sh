6dbc75249f9ee0ea58e335112eb7f9537e654bde
Vlad Minaev <minaev@haulmont.com>
2017-08-01 13:03:53 +0400

    Get rid of git modules

---
+++ clone-all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+BASE_URL="https://github.com/cuba-elements/"
+
+git clone $BASE_URL"cuba-app.git" "../cuba-app"
+git clone $BASE_URL"cuba-data.git" "../cuba-data"
+git clone $BASE_URL"cuba-form.git" "../cuba-form"
+git clone $BASE_URL"cuba-login.git" "../cuba-login"
+git clone $BASE_URL"cuba-file-field.git" "../cuba-file-field"
+git clone $BASE_URL"cuba-ui.git" "../cuba-ui"
+git clone $BASE_URL"cuba-styles.git" "../cuba-styles"
